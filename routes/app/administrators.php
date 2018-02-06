56291b7adcdd8d3f1eecbff9039a52c6c3f6f6ee
Sebastian Fix <sebastian.fix@smartgate.ch>
2018-02-06 16:39:21 +0100

    Refactored routes, implemented route groups

---
+++ routes/app/administrators.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::group(['prefix' => '/administrators','namespace' => 'Controllers\Administrators\Dashboard','as' => 'administrators.'], function () {
+
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard.index');
+
+});
---
+++ routes/app/moderators.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::group(['prefix' => '/moderators','namespace' => 'Controllers\Moderators\Dashboard','as' => 'moderators.'], function () {
+
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard.index');
+
+});
