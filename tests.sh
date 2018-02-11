bc202bc17b915daa134ffc3f023fbf53cb39b1fc
Sebastian Fix <sebastian.fix@smartgate.ch>
2018-02-11 13:03:37 +0100

    Implemented Laravel Forge WebHook after successfull testing

---
+++ tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Trigger deployment
+# Replace the url below with your Forge/Laravel url
+curl -s 'https://forge.laravel.com/servers/180458/sites/479356/deploy/http?token=coYjkevttH3odg6R2qFd55ZCmR5u3vaka2N6ycq5';
+echo 'Deployment triggered!
