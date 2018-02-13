dab0c1e790ccab221cdbb526055d1fb8b20341a7
Sebastian Fix <sebastian.fix@smartgate.ch>
2018-02-13 07:16:47 +0100

    Updated Recommendations View

--- app/Http/Controllers/Administrators/Recommendations/RecommendationsController.php
+++ app/Http/Controllers/Administrators/Recommendations/RecommendationsController.php
@@ -33,108 +33,10 @@ class RecommendationsController extends Controller
     public function index()
     {
 
-        $this->fillPlaces();
 
-        dd('success');
+        return view('app.administrators.recommendations.index');
 
-
-        dd('save!');
-
-        $this->storeLogs();
-
-        dd('success');
-
-
-        $this->fillDevices();
-
-        dd('success');
-
-
-
-        $this->resetDatabase();
-
-        dd('deleted');
 
     }
 
-    public function resetDatabase()
-    {
-
-    }
-
-    public function getDeviceBasedRecommendation(Device $device, $count)
-    {
-        return app()->recombee->send(new UserBasedRecommendation($device->uuid, $count));
-    }
-
-
-    public function getItemBasedRecommendation(Place $place, $count)
-    {
-        return app()->recombee->send(new ItemBasedRecommendation($place->uuid, $count));
-
-    }
-
-    public function storeLogs()
-    {
-        $logs = LogPlace::where('id', '>=', 2251)
-            ->where('id', '<=', 2500)
-            ->get();
-
-        foreach ($logs as $log)
-        {
-      }
-
-    }
-
-
-    public function fillDevices()
-    {
-        $devices = Device::where('id', '>=', 451)
-                ->where('id', '<=', 550)
-                ->get();
-
-            foreach ($devices as $device)
-            {
-                $this->addDevice($device);
-            }
-    }
-
-    public function addDevice(Device $device)
-    {
-        app()->recombee->send(new AddUser($device->uuid));
-    }
-
-
-    public function fillPlaces()
-    {
-        $places = Place::where('id', '>=', 1)
-            ->where('id', '<=', 150)
-            ->get();
-
-        foreach ($places as $place)
-        {
-            $this->addPlace($place);
-        }
-
-        $this->setPlacesProperties();
-
-        foreach ($places as $place)
-        {
-            $this->fillPlaceProperties($place);
-        }
-    }
-
-
-
-
-    public function addUser($uuid)
-    {
-        app()->recombee->send(new AddUser($uuid));
-
-    }
-
-    public function getUsersList()
-    {
-        return app()->recombee->send(new ListUsers());
-    }
 }
