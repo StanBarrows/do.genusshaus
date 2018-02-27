21fcedd112c020860e5bcf92b3ac3e33fb5e2c41
Sebastian Fix <sebastian.fix@smartgate.ch>
2018-02-28 00:18:40 +0100

    Updated testing config

--- config/database.php
+++ config/database.php
@@ -33,26 +33,6 @@ return [
 
     'connections' => [
 
-        'testing' => [
-            'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', '127.0.0.1'),
-            'port'     => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'travis'),
-            'username' => env('DB_USERNAME', 'postgres'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset'  => 'utf8',
-            'prefix'   => '',
-            'schema'   => 'public',
-            'sslmode'  => 'prefer',
-        ],
-
-
-        'sqlite' => [
-            'driver'   => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix'   => '',
-        ],
-
         'mysql' => [
             'driver'      => 'mysql',
             'host'        => env('DB_HOST', '127.0.0.1'),
