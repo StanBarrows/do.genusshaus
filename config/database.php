2171a35f79d2a3fa3863aa6ed5509d1a251a9e6a
Sebastian Fix <sebastian.fix@smartgate.ch>
2018-02-27 01:01:09 +0100

    Updated Travis Testing

--- config/database.php
+++ config/database.php
@@ -32,6 +32,24 @@ return [
     */
 
     'connections' => [
+
+
+        'travis' => [
+            'driver'      => 'mysql',
+            'host'        => env('DB_HOST', '127.0.0.1'),
+            'port'        => env('DB_PORT', '3306'),
+            'database'    => env('DB_DATABASE', 'travis'),
+            'username'    => env('DB_USERNAME', 'homestead'),
+            'password'    => env('DB_PASSWORD', 'secret'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset'     => 'utf8mb4',
+            'collation'   => 'utf8mb4_unicode_ci',
+            'prefix'      => '',
+            'strict'      => true,
+            'engine'      => null,
+        ],
+
+
         'sqlite' => [
             'driver'   => 'sqlite',
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
